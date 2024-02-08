658b725e6658c60d7d000ba61c4b4a35bc2f7d0a
Amirkhan <hisam18@mail.ru>
2024-02-08 13:43:15 +0300

    Сделал первое задание

--- ISRPO2/Controllers/HomeController.cs
+++ ISRPO2/Controllers/HomeController.cs
@@ -28,5 +28,32 @@ namespace ISRPO2.Controllers
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
-	}
+		public IActionResult TaskFirst()
+		{
+			return View();
+		}
+        public IActionResult TaskSecond()
+        {
+            return View();
+        }
+        public IActionResult TaskThird()
+        {
+            return View();
+        }
+		[HttpPost]
+		public IActionResult TaskFirst(string firstNum, string secondNum, string thirdNum)
+		{
+			int result;
+			if (Convert.ToInt32(firstNum) > 0 && Convert.ToInt32(secondNum) > 0 && Convert.ToInt32(thirdNum) > 0 && firstNum == null && secondNum == null && thirdNum == null)
+			{
+				result = (Convert.ToInt32(firstNum) + Convert.ToInt32(secondNum) + Convert.ToInt32(thirdNum)) / 3;
+			}
+			else
+			{
+				result = Convert.ToInt32(firstNum) * Convert.ToInt32(secondNum) * Convert.ToInt32(thirdNum);
+			}
+			ViewBag.H = Convert.ToInt32(result);
+			return View();
+		}
+    }
 }
